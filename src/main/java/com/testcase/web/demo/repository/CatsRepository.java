0d8c0d5e086b9b9e35eb3bdf8622db14805d22d9
Alina Sokolova <fkathryn@student.21-school.ru>
2021-04-22 23:55:46 +0300

    query value?

--- src/main/java/com/testcase/web/demo/repository/CatsRepository.java
+++ src/main/java/com/testcase/web/demo/repository/CatsRepository.java
@@ -2,11 +2,15 @@ package com.testcase.web.demo.repository;
 
 import com.testcase.web.demo.entity.Cats;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CatsRepository extends JpaRepository<Cats, Integer> {
     Cats findByName(String name);
     Optional<Cats> findById(Integer id);
+    @Query(value = "SELECT * FROM cats_table ORDER BY RAND()", nativeQuery = true)
     Iterable<Integer> findTopById();
 }
