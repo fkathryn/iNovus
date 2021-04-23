46565b4a72a5fbcb9aec2cf3516d6b04642ed975
Alina Sokolova <fkathryn@student.21-school.ru>
2021-04-23 20:08:32 +0300

    TestClasses

--- src/test/java/com/testcase/web/demo/repository/CatsRepositoryTest.java
+++ src/test/java/com/testcase/web/demo/repository/CatsRepositoryTest.java
@@ -1,4 +1,50 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.testcase.web.demo.repository;
+
+import com.testcase.web.demo.entity.Cats;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.sql.DriverManager;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class CatsRepositoryTest {
-  
+    @Autowired
+    private CatsRepository catsRepository;
+
+    @Test
+    void findAll() {
+        System.out.println(catsRepository.findAll().size());
+    }
+
+
+    @Test
+    Cats findByName(String name) {
+        Cats cats = findByName(name);
+        Assertions.assertEquals(cats.getName(), "Rick");
+        Assertions.assertEquals(cats.getId(), 1);
+        System.out.println(cats.getName());
+
+        return cats;
+    }
+
+    @Test
+    void findById() {
+        Cats cat = catsRepository.findById(2).get();
+        System.out.println(cat);
+    }
+
+    @Test
+    void findCatsByRand() {
+    }
+
+    @Test
+    void findTopById() {
+    }
 }
--- src/test/java/com/testcase/web/demo/service/impl/CatsServiceImplTest.java
+++ src/test/java/com/testcase/web/demo/service/impl/CatsServiceImplTest.java
@@ -1,4 +1,37 @@
+package com.testcase.web.demo.service.impl;
+
+import com.testcase.web.demo.entity.dto.CatsDto;
+import com.testcase.web.demo.repository.CatsRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class CatsServiceImplTest {
-  
+
+    @Autowired
+    private CatsServiceImpl catsService;
+
+    @Test
+    void findAll() {
+        System.out.println(catsService.findAll().size());
+    }
+
+    @Test
+    void getTopCats() {
+        List<CatsDto> catsList = catsService.getTopCats();
+        assertEquals(catsList.size(), 4);
+    }
+
+    @Test
+    void getPairCats() {
+    }
 }
